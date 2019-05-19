b064d33d42b3fa1334431e9a4a7bb1e1f53e5163
rolandolucio <rolandolucio@gmail.com>
2019-05-19 06:09:39 -0500

    tv socket scrap ok

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict';
+// https://stackoverflow.com/questions/48375700/how-to-use-puppeteer-to-dump-websocket-data
 /*
 https: //github.com/GoogleChrome/puppeteer/issues/3547
 Here 's how I capture web socket by Puppeteer and CDP.
@@ -18,9 +19,36 @@ const handleWebSocketFrameReceived = (params) => {
 }
 */
 
-
-
-//const config = require('./lib/auto-load')(__dirname + '/config');
+const puppeteer = require('puppeteer');
 const config =require('./config');
+const configP={
+  headless:false,
+  defaultViewport:{
+    width:1340,
+    height:750
+  }
+};
+//console.log(JSON.stringify(config, null, 2));
+(async ()=>{
+  const browser = await puppeteer.launch(configP);
+  const page = (await browser.pages())[0];
 
-console.log(JSON.stringify(config, null, 2));
+  const f12 = await page.target()
+    .createCDPSession();
+  await f12.send('Network.enable');
+  await f12.send('Page.enable');
+
+  const handleWebSocketFrameReceived = (params) => {
+    const payload = params.response.payloadData != undefined ? params.response.payloadData:{};
+    console.log(params);
+    console.log(payload);
+  };
+  const chartUrl = `${config.tv.site}/chart/${config.tv.chart.id}/`;
+  console.log(chartUrl);
+  await page.goto(chartUrl, { waitUntil: 'domcontentloaded' });
+  await page.screenshot({ path: `./log/captures/${Date.now()}.png` });
+  f12.on('Network.webSocketFrameReceived', handleWebSocketFrameReceived);
+  
+  
+})();
+
